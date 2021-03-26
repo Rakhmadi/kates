a698acc3d7a30e4c76c1edfb18b8d1750848e0e6
rakhmadi <rakhmadiwalker@gmail.com>
2021-03-27 06:00:23 +0700

    commit

---
+++ kates.js
@@ -0,0 +1,11 @@
+let kates = {
+    toLink(link){
+        location.href = link
+    },
+    showHide(id){
+        document.getElementById('')
+    },
+    toLinkOpen(link,action='_blank',conf=''){
+        window.open(link,action,conf)
+    }
+}
